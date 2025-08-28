f9615b56e6080adacce6ce808c68c4cb792977d1
ZapolyarnyDev <zapolyarnynorth@gmail.com>
2025-08-28 10:15:36 +0300

    (easy) 100. Same Tree

---
+++ easy/100. Same Tree/Solution.kt
@@ -0,0 +1,8 @@
+class Solution {
+    fun isSameTree(p: TreeNode?, q: TreeNode?): Boolean {
+        if(p == q) return true
+        if(p == null || q == null) return false
+        
+        return p.`val` == q.`val` && isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
+    }
+}
